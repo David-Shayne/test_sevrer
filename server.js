2d8a057a41dd36f4e7b31073a95d742b257cb2ef
David <dmshayne@gmail.com>
2020-10-25 12:43:02 +0200

    .

--- server.js
+++ server.js
@@ -1,9 +1,5 @@
 const express = require('express');
 const app = express();
-app.use(express.json());
-app.post('/api', (req, res) => {
-  res.send(req.body);
-});
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
